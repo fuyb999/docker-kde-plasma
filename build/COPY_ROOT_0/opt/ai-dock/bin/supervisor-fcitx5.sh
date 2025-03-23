2478108e29914d17f9631160136b36cb09619415
abc <you@example.com>
2025-03-23 21:10:34 +0800

    fix fcitx5 autostart

--- build/COPY_ROOT_0/opt/ai-dock/bin/supervisor-fcitx5.sh
+++ build/COPY_ROOT_0/opt/ai-dock/bin/supervisor-fcitx5.sh
@@ -10,29 +10,24 @@ function cleanup() {
 }
 
 function autostart() {
-    print_header "Configure Fcitx5"
 
-    fcitx5_autostart_desktop="$(cat <<EOF
-    [Desktop Entry]
-    Encoding=UTF-8
-    Type=Application
-    Name=Fctix5
-    Comment=Launch fctix5 on login
-    Exec=/usr/bin/fcitx5 %U
-    Icon=fcitx
-    RunHook=0
-    StartupNotify=false
-    Terminal=false
-    Hidden=false
-    EOF
+    fcitx5_autostart_desktop="$(cat << EOF
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=Fctix5
+Comment=Launch fctix5 on login
+Exec=/usr/bin/fcitx5 %U
+Icon=fcitx
+RunHook=0
+StartupNotify=false
+Terminal=false
+Hidden=false
+EOF
     )"
 
-    print_step_header "Enable Fcitx5 auto-start script"
-    mkdir -p "${USER_HOME:?}/.config/autostart"
-    echo "${fcitx5_autostart_desktop:?}" > "${USER_HOME:?}/.config/autostart/Fcitx5.desktop"
-    #sed -i 's|^autostart.*=.*$|autostart=false|' /etc/supervisor.d/fcitx5.ini
-
-    echo -e "\e[34mDONE\e[0m"
+    mkdir -p "${HOME:?}/.config/autostart"
+    echo "${fcitx5_autostart_desktop:?}" > "${HOME:?}/.config/autostart/Fcitx5.desktop"
 }
 
 function start() {
