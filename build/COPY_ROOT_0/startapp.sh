9482260f7fb26546f62c3833beb3744632b6efb7
develop <a@b.com>
2025-09-05 17:04:15 +0800

    success

--- build/COPY_ROOT_0/startapp.sh
+++ build/COPY_ROOT_0/startapp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 禁用unicode,chttrans(简繁切换)插件，以解决ctrl+shift+f,ctrl+shift+u快捷键冲突
-fcitx5 -d --disable=wayland,unicode,chttrans,cloudpinyin --keep
-
+if command -v fcitx5; then
+  fcitx5 -d --disable=wayland,unicode,chttrans,cloudpinyin --keep
+fi
 exec ${STARTAPP:-tail -f /dev/null}
