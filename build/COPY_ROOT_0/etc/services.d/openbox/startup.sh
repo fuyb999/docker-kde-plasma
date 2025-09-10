eb6380aefa535dab10e25f73fe8a02ba05be04e6
develop <a@b.com>
2025-09-10 22:43:44 +0800

    update

--- build/COPY_ROOT_0/etc/services.d/openbox/startup.sh
+++ build/COPY_ROOT_0/etc/services.d/openbox/startup.sh
@@ -4,11 +4,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 source /opt/ai-dock/etc/environment.sh
+
 tail -f /dev/null
-#if ! command -v fcitx5; then
-#  sudo tar -Jxvf /workspace/fcitx5-v5.1.11.tar.xz -C /
-#fi
-#
-#exec fcitx5
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
