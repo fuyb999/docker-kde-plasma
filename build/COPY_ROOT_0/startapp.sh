2a14590df670503851d91c805baa84f241cd059e
develop <a@b.com>
2025-09-11 22:44:51 +0800

    update

--- build/COPY_ROOT_0/startapp.sh
+++ build/COPY_ROOT_0/startapp.sh
@@ -8,7 +8,7 @@ fi
 
 # 禁用unicode,chttrans(简繁切换)插件，以解决ctrl+shift+f,ctrl+shift+u快捷键冲突
 if command -v fcitx5; then
-  fcitx5 --disable=unicode,chttrans -d --keep
+  fcitx5 --disable=unicode,chttrans -d --keep --verbos 5
 fi
 
 exec ${STARTAPP:-tail -f /dev/null}
