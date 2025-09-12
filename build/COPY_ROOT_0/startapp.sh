84ee1e2b300632e1440b228fbf4b9ea54c07324c
develop <a@b.com>
2025-09-12 23:19:20 +0800

    update

--- build/COPY_ROOT_0/startapp.sh
+++ build/COPY_ROOT_0/startapp.sh
@@ -2,9 +2,9 @@
 
 source /opt/ai-dock/etc/environment.sh
 
-if ! command -v fcitx5; then
-  sudo tar -Jxf ${WORKSPACE}/addons/softwares/fcitx5-v5.1.11.tar.xz -C /
-fi
+#if ! command -v fcitx5; then
+#  sudo tar -Jxf ${HOME}/addons/softwares/fcitx5-v5.1.11.tar.xz -C /
+#fi
 
 # 禁用unicode,chttrans(简繁切换)插件，以解决ctrl+shift+f,ctrl+shift+u快捷键冲突
 if command -v fcitx5; then
--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -3,4 +3,50 @@
 # This file will be sourced in init.sh
 # You can edit below here and make it do something useful
 
+XDG_SOFTWARE_HOME=/opt/apps
+PKG_HOME=$HOME/addons
+DBEAVER_VERSION=ee-24.2.0
+JAVA_HOME=$XDG_SOFTWARE_HOME/jdk-17.0.10
+
+DBEAVER_HOME=${XDG_SOFTWARE_HOME}/dbeaver-${DBEAVER_VERSION}
+
+install(){
+  if [ -d "$DBEAVER_HOME" ] && [ -f "$DBEAVER_HOME/dbeaver" ]; then
+    ln -sf $DBEAVER_HOME/dbeaver /usr/local/bin/dbeaver
+  fi
+}
+
+install
+if [ ${ENABLE_DBEAVER} -eq 0 ] || [ -n "$(which dbeaver)" ]; then
+  exit 0
+fi
+
+if [ ! -f "${PKG_HOME}/softwares/dbeaver-${DBEAVER_VERSION}-linux.gtk.x86_64.tar.gz" ]; then
+  # ${DBEAVER_VERSION:3} -> ce_24.1.2 -> 24.1.2
+ # wget https://dbeaver.com/downloads-ultimate/${DBEAVER_VERSION:3}/dbeaver-${DBEAVER_VERSION}_amd64.deb -O ${PKG_HOME}/dbeaver-${DBEAVER_VERSION}_amd64.deb
+ wget https://dbeaver.com/files/${DBEAVER_VERSION:3}/dbeaver-${DBEAVER_VERSION}-linux.gtk.x86_64.tar.gz -O ${PKG_HOME}/softwares/dbeaver-${DBEAVER_VERSION}-linux.gtk.x86_64.tar.gz
+fi
+
+mkdir -p $DBEAVER_HOME
+#sudo dpkg -i ${PKG_HOME}/dbeaver-${DBEAVER_VERSION}_amd64.deb
+tar --strip-components=1 -zxf ${PKG_HOME}/softwares/dbeaver-${DBEAVER_VERSION}-linux.gtk.x86_64.tar.gz -C $DBEAVER_HOME
+install
+
+# https://github.com/wgzhao/dbeaver-agent
+if [ -f "$PKG_HOME/crack/dbeaver/dbeaver-agent-1.0.jar" ] && [ ! -f "$DBEAVER_HOME/dbeaver-agent-1.0.jar" ]; then
+  sudo cp $PKG_HOME/crack/dbeaver/dbeaver-agent-1.0.jar $DBEAVER_HOME/dbeaver-agent.jar
+fi
+
+DBEAVER_CONFIG=$DBEAVER_HOME/dbeaver.ini
+if [ -z "$(grep dbeaver-agent $DBEAVER_CONFIG)" ]; then
+  sudo sed -i -e "/-vmargs/a\-javaagent:${DBEAVER_HOME}/dbeaver-agent.jar" $DBEAVER_CONFIG
+fi
+
+sudo rm -rf $DBEAVER_HOME/jre && sudo ln -s $JAVA_HOME $DBEAVER_HOME/jre
+$JAVA_HOME/bin/java -cp $PKG_HOME/crack/dbeaver/libs/\*:$DBEAVER_HOME/dbeaver-agent.jar \
+   dev.misakacloud.dbee.License \
+   --product=dbeaver \
+   --type=ee \
+   --version=24
+
 printf "Hello world!\n"
