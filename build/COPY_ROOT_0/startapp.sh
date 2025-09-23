ce4c4fb114e1483be559a110e9dcd53b3ac1932a
fuyb <656452024@qq.com>
2025-09-23 19:11:15 +0800

    update

--- build/COPY_ROOT_0/startapp.sh
+++ build/COPY_ROOT_0/startapp.sh
@@ -2,13 +2,13 @@
 
 source /opt/ai-dock/etc/environment.sh
 
-#if ! command -v fcitx5; then
-#  sudo tar -Jxf ${HOME}/addons/softwares/fcitx5-v5.1.11.tar.xz -C /
-#fi
+if ! command -v fcitx5; then
+  sudo tar -Jxf ${SOFTWARE_ADDONS_DIR}/fcitx5-v5.1.11.tar.xz -C /
+fi
 
-if [ ! -d "~/.config" ]; then
-  sudo cp -r /root/.config ~/
-  sudo chown -R ${USER_ID}:${GROUP_ID} ~/.config
+if [ ! -d "${XDG_CONFIG_HOME}" ]; then
+  sudo cp -r /root/.config "$(basedir $XDG_CONFIG_HOME)"
+  sudo chown -R ${USER_ID}:${GROUP_ID} ${XDG_CONFIG_HOME}
 fi
 
 # 禁用unicode,chttrans(简繁切换)插件，以解决ctrl+shift+f,ctrl+shift+u快捷键冲突
