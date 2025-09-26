7963d9d91d0538c6b791a84ddb5804501ff4a1c0
fuyb <656452024@qq.com>
2025-09-26 23:41:35 +0800

    update

--- build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/init.sh
+++ build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/init.sh
@@ -4,17 +4,48 @@
 set -eo pipefail
 umask 002
 
-source /opt/ai-dock/bin/build/layer0/common.sh
+groupadd -g 1111 ai-dock
+chown root.ai-dock /opt
+chmod g+w /opt
+chmod g+s /opt
 
-if [[ "$XPU_TARGET" == "NVIDIA_GPU" ]]; then
-    source /opt/ai-dock/bin/build/layer0/nvidia.sh
-elif [[ "$XPU_TARGET" == "AMD_GPU" ]]; then
-    source /opt/ai-dock/bin/build/layer0/amd.sh
-elif [[ "$XPU_TARGET" == "CPU" ]]; then
-    source /opt/ai-dock/bin/build/layer0/cpu.sh
-else
-    printf "No valid XPU_TARGET specified\n" >&2
-    exit 1
-fi
+mkdir -p /opt/environments/{python,javascript}
 
+# Prepare environment for running SSHD
+chmod 700 /root
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+touch /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
+# Remove less relevant parts of motd
+rm -f /etc/update-motd.d/10-help-text
+
+# Ensure critical paths/files are present
+mkdir -p --mode=0755 /etc/apt/keyrings
+mkdir -p --mode=0755 /run/sshd
+chown -R root.ai-dock /var/log
+chmod -R g+w /var/log
+chmod -R g+s /var/log
+mkdir -p /var/log/supervisor
+mkdir -p /var/empty
+mkdir -p /etc/rclone
+touch /etc/rclone/rclone.conf
+
+printf "source /opt/ai-dock/etc/environment.sh\n" >> /etc/profile.d/02-ai-dock.sh
+printf "source /opt/ai-dock/etc/environment.sh\n" >> /etc/bash.bashrc
+printf "ready-test\n" >> /root/.bashrc
+
+#if [[ "$XPU_TARGET" == "NVIDIA_GPU" ]]; then
+#    source /opt/ai-dock/bin/build/layer0/nvidia.sh
+#elif [[ "$XPU_TARGET" == "AMD_GPU" ]]; then
+#    source /opt/ai-dock/bin/build/layer0/amd.sh
+#elif [[ "$XPU_TARGET" == "CPU" ]]; then
+#    source /opt/ai-dock/bin/build/layer0/cpu.sh
+#else
+#    printf "No valid XPU_TARGET specified\n" >&2
+#    exit 1
+#fi
+
+# Give our runtime user full access (added to ai-dock group)
 source /opt/ai-dock/bin/build/layer0/clean.sh
