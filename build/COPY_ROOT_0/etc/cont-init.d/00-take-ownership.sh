b09ce2d3756163a714f34245ecf1e135cb000b73
fuyb <656452024@qq.com>
2025-09-27 17:34:53 +0800

    update

--- build/COPY_ROOT_0/etc/cont-init.d/00-take-ownership.sh
+++ build/COPY_ROOT_0/etc/cont-init.d/00-take-ownership.sh
@@ -8,7 +8,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 sudo chown ${USER_ID}:${GROUP_ID} ${HOME}
-sudo chown -R ${USER_ID}:ai-dock \
+sudo chown -R ${USER_ID}:${GROUP_ID} \
      /opt/ai-dock \
      ${XDG_SOFTWARE_HOME:-/opt/apps} \
      ${XDG_ADDONS_HOME:-/opt/addons}
